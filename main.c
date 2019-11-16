1f412a7374e11a4d743cb2edc6e62eae89e650a1
arvicrin <1231@holbertonschool.com>
2019-11-16 22:58:17 +0000

    v1.1 main

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 ssize_t _getline(char **buffer, size_t *sizebuf, int *stream)
 {
 	ssize_t status;
 
-	*buffer = malloc(sizeof(char) * 120);
+	*buffer = malloc(sizeof(char) * (*sizebuf));
 	
 	if (*buffer == NULL)
 	{
@@ -14,7 +15,7 @@ ssize_t _getline(char **buffer, size_t *sizebuf, int *stream)
 		return (0);
 	}
 
-	status = read(*stream, *buffer, *sizebuf);
+	status = read(0, *buffer, *sizebuf);
 	
 	if (status == -1)
 	{
@@ -22,20 +23,68 @@ ssize_t _getline(char **buffer, size_t *sizebuf, int *stream)
 		return (0);
 	}
 
-	/*(*buffer)[status] = '\0';*/
+	(*buffer)[status] = '\0';
 
-	/*printf("%s, %zd\n", *buffer, status);*/
+	printf("%s, %zd\n", *buffer, status);
 
 	return(status);
 }
 
 int main(void)
 {
-	size_t status = 0, sizebuf = 120;
-	char *buffer = NULL;
+	ssize_t status = 0, sizebuf = 20;
+	char *buffer = NULL, *copybuffer = NULL, *token, **tokens;
+	const char delim[2] = " ";
+	int i = 0;
 
+	/*Our shell prompt*/
 	printf("$ ");
-	status = _getline(&buffer, &sizebuf, STDIN_FILENO);
+
+	/* READ part*/
+	status = getline(&buffer, &sizebuf, stdin);
+	if (status == -1)
+	{
+	        printf("Error\n");
+		return (0);
+	}
+
+	printf("Read part: %s, %zd\n", buffer, status);
+	
+	/*copybuffer*/
+	copybuffer = malloc(sizeof(char) * status + 1);
+	for (i = 0; buffer[i] != '\0'; i++)
+		copybuffer[i] = buffer[i];
+	copybuffer[i] = '\0';
+
+	printf("Copybuffer: %s, %i\n", copybuffer, i);
+
+	token = strtok(copybuffer, delim);
+	for (i = 0; token != NULL; i++)
+		token = strtok(NULL, delim);
+	free(copybuffer);
+
+	/*Parse part*/
+	tokens = malloc(sizeof(char *) * i);
+	if (tokens == NULL)
+	{
+		printf("Error\n");
+		return (0);
+	}
+
+	token = strtok(buffer, delim);
+	printf("Parse part: ");
+
+	for (i = 0; token != NULL; i++)
+	{
+		tokens[i] = token;
+		printf("%s, ", tokens[i]);
+	        token = strtok(NULL, delim);
+	}
+
+	printf("\n");
+
+
 
 	free(buffer);
+	free(tokens);
 }
