c38ed93ead7bdce5965bc0c673e2b48b0b9de7e5
juanfe9118 <1168@holbertonschool.com>
2019-11-19 19:41:54 +0000

    Modify header

--- headersh.h
+++ headersh.h
@@ -20,5 +20,6 @@ void parsesh(char **buffer, int *len, char ***tokens, int *status);
 void createandexesh(char ***tokens, int *status);
 void _strtok(void);
 size_t _getline(char **buffer, size_t *sizebuf, int *stream);
-char *_getenv(const char *variable_env);
+char *_getenv(const char *variable_env);}
+path_node *_getpathdir(char *path);
 #endif
